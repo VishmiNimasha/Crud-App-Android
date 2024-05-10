d136b086f1d0e976034d522861a16ed13f5b3420
Nimasha <nimashavithanage25@gmail.com>
2024-05-11 00:09:56 +0530

    fifth

--- app/src/main/java/com/example/madcrudapp/Note.kt
+++ app/src/main/java/com/example/madcrudapp/Note.kt
@@ -1,3 +1,5 @@
 package com.example.madcrudapp
 
 data class Note(val id:Int, val title: String, val content: String)
+
+
--- app/src/main/java/com/example/madcrudapp/NotesDatabaseHelper.kt
+++ app/src/main/java/com/example/madcrudapp/NotesDatabaseHelper.kt
@@ -46,17 +46,44 @@ class NotesDatabaseHelper (context:Context):SQLiteOpenHelper(context,DATABASE_NA
 
         while(cursor.moveToNext()){
             val id = cursor.getInt(cursor.getColumnIndexOrThrow(COLUMN_ID))
-            val title = cursor.getInt(cursor.getColumnIndexOrThrow(COLUMN_TITLE))
-            val content = cursor.getInt(cursor.getColumnIndexOrThrow(COLUMN_CONTENT))
+            val title = cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_TITLE))
+            val content = cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_CONTENT))
 
-           val note = Note(id, title, content)
+            val note = Note(id, title, content)
             notesList.add(note)
-
-
         }
-
         cursor.close()
         db.close()
         return notesList
     }
+
+    fun updateNote(note:Note){
+        val db = writableDatabase
+        val values = ContentValues().apply{
+            put(COLUMN_TITLE,note.title)
+            put(COLUMN_CONTENT,note.content)
+        }
+
+        val whereClause = "$COLUMN_ID = ?"
+        val whereArgs = arrayOf(note.id.toString())
+        db.update(TABLE_NAME, values,whereClause,whereArgs)
+        db.close()
+    }
+    
+    fun getNoteByID(noteId:Int):Note{
+        val db = readableDatabase
+        val query = "SELECT = FROM $TABLE_NAME WHERE $COLUMN_ID = $noteId"
+        val cursor = db.rawQuery(query,null )
+        cursor.moveToFirst()
+
+        val id = cursor.getInt(cursor.getColumnIndexOrThrow(COLUMN_ID))
+        val title = cursor.getInt(cursor.getColumnIndexOrThrow(COLUMN_TITLE))
+        val content = cursor.getInt(cursor.getColumnIndexOrThrow(COLUMN_CONTENT))
+
+
+        cursor.close()
+        db.close()
+        return Note(id, title.toString(), content.toString())
+    }
+
 }
